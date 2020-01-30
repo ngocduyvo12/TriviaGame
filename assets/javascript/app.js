831369b86eb3cf672fee46e461f2505b8a714aa1
Ngoc Vo <ngocduyvo12@yahoo.com>
2020-01-30 02:26:44 -0700

    removed debugging console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -102,7 +102,9 @@ $(".startButton").on("click", function () {
     //function to create question when start is clicked
     function createQuestions() {
         for (var i = 0; i < questions.questions.length; i++) {
-            console.log(questions.questions[i])
+
+            // console.log(questions.questions[i])
+
             $("#formBody").append(`<div class="question"><h3>${questions.questions[i]}</h3><div>`)
             console.log(nameArray[i])
             for (var k = 0; k < 4; k++) {
